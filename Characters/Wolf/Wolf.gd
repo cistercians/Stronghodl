ec78138ab0dccf5e32ed16ac1d7e250aa3bf1e8c
Johan <jonargyne@gmail.com>
2024-04-12 14:10:29 -0700

    npcs

--- Characters/Wolf/Wolf.gd
+++ Characters/Wolf/Wolf.gd
@@ -4,10 +4,12 @@ extends CharacterBody2D
 
 func _process(_delta):
 	_animated_sprite.play("Stand_D")
+	
+func _physics_process(delta):
+	move_and_slide()
 
 func _on_detection_body_entered(body):
 	pass # Replace with function body.
-
 
 func _on_detection_body_exited(body):
 	pass # Replace with function body.
--- States/Idle.gd
+++ States/Idle.gd
@@ -9,7 +9,7 @@ var wander_time: float
 
 func randomize_wander():
 	move_direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
-	wander_time = randf_range(1, 3)
+	wander_time = randf_range(5, 10)
 	
 func Enter():
 	randomize_wander()
