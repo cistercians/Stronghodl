4f19b8cde24a10a94ecc3b456937e3cbf166d9d6
Johan <jonargyne@gmail.com>
2024-04-17 14:27:12 -0700

    npcs

--- States/Idle.gd
+++ States/Idle.gd
@@ -2,7 +2,7 @@ extends State
 class_name Idle
 
 @export var char: CharacterBody2D
-@export var move_speed:= 30
+@export var move_speed:= 40
 
 var move_direction: Vector2
 var wander_time: float
@@ -10,7 +10,7 @@ var wander_cooldown: float
 
 func randomize_wander():
 	move_direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
-	wander_time = randf_range(5, 10)
+	wander_time = randf_range(3, 5)
 	wander_cooldown = randf_range(5, 10)
 	char.moving = 1
 	
@@ -27,6 +27,5 @@ func Update(delta: float):
 		randomize_wander()
 		
 func Physics_Update(delta: float):
-	if char:
-		char.velocity = move_direction * move_speed
-		char.update_direction(move_direction)
+	char.update_direction(move_direction)
+	char.velocity = move_direction * move_speed
